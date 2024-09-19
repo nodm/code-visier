ecb32c3047f7a45ba5c25a71ba03ed059ef03479
Dmytro Novikov <nodm.pe@gmail.com>
2024-09-19 22:35:15 +0300

    Generate tests for an active file or for selected content

--- src/extension.ts
+++ src/extension.ts
@@ -13,20 +13,30 @@ export function activate(context: vscode.ExtensionContext) {
       }
 
       const document = activeEditor.document;
-      const content = document.getText();
+      const fileName = document.uri.path.split('/').pop();
+
+      const selection = activeEditor.selection;
+      const content = selection.isEmpty
+        ? document.getText()
+        : document.getText(selection);
 
       const message = {
         role: 'user',
         content: `
-				  You are a seasoned developer.
+				  Act as a seasoned developer.
 					You have expertise in JavaScript, TypeScript, React, Redux, Redux ToolKit, Redux Toolkit Query, Jest, React Testing Library.
 					Create tests for the following code snippet:
 
-					<code>
+
+          <code>
+            // File: ${fileName}
 					  ${content}
 					</code>
 
-					Provide maximum coverage for statements, branches, and functions. Make sure to test all edge cases.
+
+          Provide maximum coverage for statements, branches, and functions. Make sure to test all edge cases.
+          Use TypeScript for writing tests.
+          Provide just the code for the tests in your response, omit the comments.
 				`,
       };
 
@@ -34,20 +44,27 @@ export function activate(context: vscode.ExtensionContext) {
         const response = await ollama.chat({
           model: 'deepseek-coder:6.7b',
           messages: [message],
-          // stream: true,
           options: {
             temperature: 0.1,
           },
         });
 
-        // for await (const part of response) {
-        //   process.stdout.write(part.message.content);
-        // }
+        vscode.window.showInformationMessage(response.message.content);
+
+        const regex = /```typescript([\s\S]*?)```/;
+        const match = response.message.content.match(regex);
+        const extractedCode = match ? match[1].trim() : '';
+
+        if (!extractedCode) {
+          vscode.window.showWarningMessage(response.message.content);
+
+          return;
+        }
 
         // Insert text at the end of the active document
         const endPosition = document.lineAt(document.lineCount - 1).range.end;
         activeEditor.edit((editBuilder) => {
-          editBuilder.insert(endPosition, `\n${response.message.content}\n`);
+          editBuilder.insert(endPosition, `\n${extractedCode}\n`);
         });
       } catch (error) {
         const message =
